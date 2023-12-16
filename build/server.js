13718a672059c8e468b9f0ee5a2e71c8e5b81d70
ceasar28 <emmanuelugochukwuekete@gmail.com>
2023-12-16 22:52:48 +0100

    cron job

--- build/server.js
+++ build/server.js
@@ -7,8 +7,9 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
+const cron_1 = require("./cron");
 const botRoute_1 = __importDefault(require("./routes/botRoute"));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = (0, express_1.default)();
 // Enable CORS for all routes
 app.use((0, cors_1.default)());
@@ -20,6 +21,7 @@ app.use("/api/bot", botRoute_1.default);
 app.use("/", (req, res) => {
     res.send("ping");
 });
+(0, cron_1.keepServerAlive)("https://auxi-bot.onrender.com");
 app.listen(PORT, () => {
     console.log(`Server is up and running on ${PORT}`);
 });
